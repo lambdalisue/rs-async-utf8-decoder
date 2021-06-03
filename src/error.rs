fca63f2c011ff8da9f6332b800efce0d7c04938e
Alisue <lambdalisue@hashnote.net>
2021-06-03 21:18:20 +0900

    Return `Poll::Ready(None)` when upstream is closed
    Close #3

--- src/error.rs
+++ src/error.rs
@@ -2,6 +2,9 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum DecodeError {
+    #[error("incomplete utf8 sequence `{0:?}`")]
+    IncompleteUtf8Sequence(Vec<u8>),
+
     #[error(transparent)]
     Utf8Error(#[from] std::str::Utf8Error),
 
